e696598d775fb00518992f69108eb3dd902bfdba
SebastianChishiri <sebastianchishiri@gmail.com>
2022-08-23 00:36:28 +0200

    push

--- 0x00-python-hello_world/4-print_float.py
+++ 0x00-python-hello_world/4-print_float.py
@@ -1,3 +1,7 @@
 #!/usr/bin/python3
-number = 3.14159
-print("Float: {:0.2f}".format(number))
+import numbers
+from tokenize import Number
+
+
+Number = 3.14159
+print("Float: {:0.2f}".format(Number))
